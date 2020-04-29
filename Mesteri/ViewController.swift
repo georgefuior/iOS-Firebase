d3a2d4c319ec9dff971d7e7f4134a281a1e8a6e0
George Fuior <george.fuior@icloud.com>
2020-04-29 11:03:34 +0300

    Initial Commit

---
+++ Mesteri/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Mesteri
+//
+//  Created by George Fuior on 29/04/2020.
+//  Copyright © 2020 George Fuior. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
